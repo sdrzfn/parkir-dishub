3e2a7b5568fe952d67a7e4a7dec1111cf038edcd
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2026-07-07 21:08:56 +0700

    mod: new utf file charset identifier

--- config/db.php
+++ config/db.php
@@ -3,4 +3,5 @@ $conn = new mysqli("127.0.0.1", "root", "", "parkir_dishub", 3306);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+$conn->set_charset('utf8mb4');
 ?>
